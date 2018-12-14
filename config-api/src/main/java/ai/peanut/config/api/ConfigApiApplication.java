0f79fb1b5de48d115f6d93157c7e58119aa3bfdd
JainWong <jian.wang@bianli24.com>
2018-12-14 19:05:27 +0800

    init

---
+++ config-api/src/main/java/ai/peanut/config/api/ConfigApiApplication.java
@@ -0,0 +1,14 @@
+package ai.peanut.config.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigApiApplication.class, args);
+    }
+
+}
+
---
+++ config-common/src/main/java/ai/peanut/config/common/ConfigCommonApplication.java
@@ -0,0 +1,14 @@
+package ai.peanut.config.common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigCommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCommonApplication.class, args);
+    }
+
+}
+
